b9bc7dd76949ada80955c6dd9ff2995afe285f65
Matt White <mattwhite1337@gmail.com>
2017-07-11 11:04:35 -0400

    basic file structure

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$.getJSON('../data/products.json', function(data){
+		//log return data
+		console.log(data);
+
+		//create and append page header from JSON
+		var pageHeader = $("<h1>");
+		pageHeader.text(data.category);
+		$(".page-header").append(pageHeader);
+	})
+});
