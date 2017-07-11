05ddeb12e8cd36bc65242493ade0aff5024c2444
Matt White <mattwhite1337@gmail.com>
2017-07-11 12:48:10 -0400

    populate the carousel with json data

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,35 @@ $(document).ready(function(){
 		var pageHeader = $("<h1>");
 		pageHeader.text(data.category);
 		$(".page-header").append(pageHeader);
-		
-	})
+
+		//populate the slider divs
+		var products = data.products;
+		for(var i = 0; i < products.length; i++){
+			var img = $("<img>");
+			img.attr('src', "../img/" + products[i].image);
+			img.attr('data-title', products[i].title);
+			img.attr('data-price', products[i].price);
+			$(".slider-for").append(img.clone());
+			$(".slider-nav").append(img.clone());
+		}
+
+	}).then(function(){
+		//run inside of then() to ensure initialization only after json data populates
+		$('.slider-for').slick({
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  arrows: false,
+		  fade: true,
+		  asNavFor: '.slider-nav'
+		});
+		$('.slider-nav').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  asNavFor: '.slider-for',
+		  dots: true,
+		  centerMode: true,
+		  focusOnSelect: true
+		});
+	});
+
 });
