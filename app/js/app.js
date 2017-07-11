1626de641494e9b4e572783261554fc6ea301bee
Matt White <mattwhite1337@gmail.com>
2017-07-11 15:14:57 -0400

    can add and remove items from cart

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+	var shoppingTotal = parseFloat(0.00);
+	var currentImg; //where we store the current item shown
+
+	function buyMe(selection){
+		if(selection.attr('data-cart') === 'no'){
+			selection.attr('data-cart', 'yes');
+			$('.buy').text("Remove Item");
+			shoppingTotal += parseFloat(selection.attr('data-price'));
+			$('.shopping-total').text("$" + parseFloat(shoppingTotal).toFixed(2));
+		}else{
+			selection.attr('data-cart', 'no');
+			$('.buy').text("Add to Cart");
+			shoppingTotal -= parseFloat(selection.attr('data-price'));
+			$('.shopping-total').text("$" + parseFloat(shoppingTotal).toFixed(2));
+		}
+	}
+
+	//set initial shopping cart value
+	$(".shopping-total").text("$" + parseFloat(shoppingTotal).toFixed(2));
+
 	$.getJSON('../data/products.json', function(data){
 		//log return data
 		console.log(data);
@@ -15,6 +35,7 @@ $(document).ready(function(){
 			img.attr('src', "../img/" + products[i].image);
 			img.attr('data-title', products[i].title);
 			img.attr('data-price', products[i].price);
+			img.attr('data-cart', 'no');
 			$(".slider-for").append(img.clone());
 			$(".slider-nav").append(img.clone());
 		}
@@ -38,4 +59,40 @@ $(document).ready(function(){
 		});
 	});
 
+	$('.slider-for').on('init', function(){
+		//on init, grab title and price of first slide
+  		var title = $("[data-slick-index='0']").attr('data-title');
+  		var price = $("[data-slick-index='0']").attr('data-price');
+  		currentImg = $("[data-slick-index='0']");
+
+  		$(".product-title").text(title);
+  		$(".price").text("$" + price);
+
+  		if($("[data-slick-index='0']").attr('data-cart') === 'no'){
+  			$('.buy').text("Add to Cart");
+  		}else {
+  			$('.buy').text("Remove Item");
+  		}
+	});
+
+	$('.slider-for').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		//before slide change, grab info for next slide
+		var title = $(slick.$slides.get(nextSlide)).attr('data-title');
+		var price = $(slick.$slides.get(nextSlide)).attr('data-price');
+		currentImg = $(slick.$slides.get(nextSlide));
+		
+		$(".product-title").text(title);
+		$(".price").text("$" + parseFloat(price).toFixed(2));
+
+		if($(slick.$slides.get(nextSlide)).attr('data-cart') === 'no'){
+  			$('.buy').text("Add to Cart");
+  		}else {
+  			$('.buy').text("Remove Item");
+  		}
+	});
+
+	$(".buy").on('click', function(event){
+		event.preventDefault();
+		buyMe(currentImg);
+	});
 });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,7 @@
-// Dependencies
+//dependencies
 var path = require("path");
 
-//Exporting routes to server.js
+//exporting routes to server.js
 module.exports = function(app){
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname,"/../public/", "index.html"));
