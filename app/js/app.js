9e5a21be7c00755a2ff5df3d27762a5e81a07630
Matt White <mattwhite1337@gmail.com>
2017-07-12 11:28:15 -0400

    ran eslint, increased font size

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,29 @@
+/* eslint-disable no-alert, no-console */
+
 $(document).ready(function(){
 	var shoppingTotal = parseFloat(0.00);
 	var currentImg; //where we store the current item shown
 
 	function buyMe(selection){
-		if(selection.attr('data-cart') === 'no'){
-			selection.attr('data-cart', 'yes');
-			$('.buy').text("Remove Item");
-			$('.buy').removeClass('buy-green').addClass('buy-black');
-			shoppingTotal += parseFloat(selection.attr('data-price'));
-			$('.shopping-total').text("$" + parseFloat(shoppingTotal).toFixed(2));
+		if(selection.attr("data-cart") === "no"){
+			selection.attr("data-cart", "yes");
+			$(".buy").text("Remove Item");
+			$(".buy").removeClass("buy-green").addClass("buy-black");
+			shoppingTotal += parseFloat(selection.attr("data-price"));
+			$(".shopping-total").text("$" + parseFloat(shoppingTotal).toFixed(2));
 		}else{
-			selection.attr('data-cart', 'no');
-			$('.buy').text("Add to Cart");
-			$('.buy').removeClass('buy-black').addClass('buy-green');
-			shoppingTotal -= parseFloat(selection.attr('data-price'));
-			$('.shopping-total').text("$" + parseFloat(shoppingTotal).toFixed(2));
+			selection.attr("data-cart", "no");
+			$(".buy").text("Add to Cart");
+			$(".buy").removeClass("buy-black").addClass("buy-green");
+			shoppingTotal -= parseFloat(selection.attr("data-price"));
+			$(".shopping-total").text("$" + parseFloat(shoppingTotal).toFixed(2));
 		}
 	}
 
 	//set initial shopping cart value
 	$(".shopping-total").text("$" + parseFloat(shoppingTotal).toFixed(2));
 
-	$.getJSON('../data/products.json', function(data){
+	$.getJSON("../data/products.json", function(data){
 		//log return data
 		console.log(data);
 
@@ -34,66 +36,66 @@ $(document).ready(function(){
 		var products = data.products;
 		for(var i = 0; i < products.length; i++){
 			var img = $("<img>");
-			img.attr('src', "../img/" + products[i].image);
-			img.attr('data-title', products[i].title);
-			img.attr('data-price', products[i].price);
-			img.attr('data-cart', 'no');
+			img.attr("src", "../img/" + products[i].image);
+			img.attr("data-title", products[i].title);
+			img.attr("data-price", products[i].price);
+			img.attr("data-cart", "no");
 			$(".slider-for").append(img.clone());
 			$(".slider-nav").append(img.clone());
 		}
 
 	}).then(function(){
 		//run inside of then() to ensure initialization only after json data populates
-		$('.slider-for').slick({
-		  slidesToShow: 1,
-		  slidesToScroll: 1,
-		  arrows: false,
-		  fade: true,
-		  asNavFor: '.slider-nav'
+		$(".slider-for").slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			fade: true,
+			asNavFor: ".slider-nav"
 		});
-		$('.slider-nav').slick({
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  asNavFor: '.slider-for',
-		  dots: true,
-		  centerMode: true,
-		  focusOnSelect: true
+		$(".slider-nav").slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			asNavFor: ".slider-for",
+			dots: true,
+			centerMode: true,
+			focusOnSelect: true
 		});
 	});
 
-	$('.slider-for').on('init', function(){
+	$(".slider-for").on("init", function(){
 		//on init, grab title and price of first slide
-  		var title = $("[data-slick-index='0']").attr('data-title');
-  		var price = $("[data-slick-index='0']").attr('data-price');
-  		currentImg = $("[data-slick-index='0']");
+		var title = $("[data-slick-index='0']").attr("data-title");
+		var price = $("[data-slick-index='0']").attr("data-price");
+		currentImg = $("[data-slick-index='0']");
 
-  		$(".product-title").text(title);
-  		$(".price").text("$" + price);
+		$(".product-title").text(title);
+		$(".price").text("$" + price);
 
-  		if($("[data-slick-index='0']").attr('data-cart') === 'no'){
-  			$('.buy').text("Add to Cart");
-  		}else {
-  			$('.buy').text("Remove Item");
-  		}
+		if($("[data-slick-index='0']").attr("data-cart") === "no"){
+			$(".buy").text("Add to Cart");
+		}else {
+			$(".buy").text("Remove Item");
+		}
 	});
 
-	$('.slider-for').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	$(".slider-for").on("beforeChange", function(event, slick, currentSlide, nextSlide){
 		//before slide change, grab info for next slide
-		var title = $(slick.$slides.get(nextSlide)).attr('data-title');
-		var price = $(slick.$slides.get(nextSlide)).attr('data-price');
+		var title = $(slick.$slides.get(nextSlide)).attr("data-title");
+		var price = $(slick.$slides.get(nextSlide)).attr("data-price");
 		currentImg = $(slick.$slides.get(nextSlide));
 		
 		$(".product-title").text(title);
 		$(".price").text("$" + parseFloat(price).toFixed(2));
 
-		if($(slick.$slides.get(nextSlide)).attr('data-cart') === 'no'){
-  			$('.buy').text("Add to Cart");
-  		}else {
-  			$('.buy').text("Remove Item");
-  		}
+		if($(slick.$slides.get(nextSlide)).attr("data-cart") === "no"){
+			$(".buy").text("Add to Cart");
+		}else {
+			$(".buy").text("Remove Item");
+		}
 	});
 
-	$(".buy").on('click', function(event){
+	$(".buy").on("click", function(event){
 		event.preventDefault();
 		buyMe(currentImg);
 	});
